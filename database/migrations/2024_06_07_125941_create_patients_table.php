f070128971a99a9cacdaa5a57d060b0da1259396
ptrcmnlgcs <gecosopatricia32@gmail.com>
2024-06-08 05:25:15 +0800

    Update migration for auth

--- database/migrations/2024_06_07_125941_create_patients_table.php
+++ database/migrations/2024_06_07_125941_create_patients_table.php
@@ -13,15 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('patients', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin', 'doctor', 'patient'])->default('patient');
-            $table->rememberToken();
+            $table->string('phone');
+            $table->text('address')->nullable();
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('patients');
     }
 };
